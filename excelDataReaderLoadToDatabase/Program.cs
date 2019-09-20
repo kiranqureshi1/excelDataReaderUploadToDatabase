d757394f75edf57888176eed184df5cbdf210d1e
kiranqureshi1 <kiranqureshioutloook.com>
2019-09-20 09:26:31 +0100

    program main and sql file has create method

---
+++ excelDataReaderLoadToDatabase/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExcelDataReader;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelDataReaderConsoleApp
+{
+    class program
+    {
+        public static string path = "C:\\Temp\\source";
+        public static string search = "*.xlsx";
+        public static String SheetName;
+        public static List<dynamic> ColumnNames;
+        public static List<dynamic> ColumnsDataTypes;
+        public static DataTable dataTable;
+        public static FileStream stream;
+        public static string[] fileEntries;
+        public static dynamic data;
+
+        //public static void DataTypesFromExcel()
+        public static void Main(string[] args)
+        {
+            fileEntries = Directory.GetFiles(path, "*" + search + "*", SearchOption.AllDirectories);
+            foreach (string fileName in fileEntries)
+            {
+                string[] fileEntries = Directory.GetFiles(path, "*" + search + "*", SearchOption.AllDirectories);
+                Sql sql = new Sql(SheetName, ColumnNames, ColumnsDataTypes, dataTable);
+                sql.CreateTable();
+            }
+        }
+    }
+}
+
---
+++ excelDataReaderLoadToDatabase/Sql.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelDataReaderConsoleApp
+{
+    class Sql
+    {
+        public object MessageBox { get; private set; }
+        public static Database db;
+        public static Table tb;
+        public static Server srv;
+        public static String SheetName;
+        public static List<dynamic> ColumnNames;
+        public static List<dynamic> ColumnsDataTypes;
+        public static DataTable DataTable;
+        public static string path = "C:\\Temp\\source";
+        public static string search = "*.xlsx";
+
+        //public Sql()
+        //{
+        //}
+
+        public Sql(String sheetName, List<dynamic> columnNames, List<dynamic> DataTypes, DataTable dataTable)
+        {
+            SheetName = sheetName;
+            ColumnNames = columnNames;
+            ColumnsDataTypes = DataTypes;
+            DataTable = dataTable;
+        }
+
+
+        public void CreateTable()
+        {
+            string[] fileEntries = Directory.GetFiles(path, "*" + search + "*", SearchOption.AllDirectories);
+            foreach (string fileName in fileEntries)
+            {
+                //Connect to the local, default instance of SQL Server.   
+                srv = new Server(".\\");
+                Console.WriteLine(srv.Name);
+                //Reference the AdventureWorks2012 database.   
+                db = srv.Databases["fileToUpload"];
+                Console.WriteLine(db.Name);
+                Console.ReadKey();
+                //Define a Table object variable by supplying the parent database and table name in the constructor.  
+                tb = new Table(db, SheetName);
+                //CreateRows();
+                tb.Create();
+            }
+
+        }
+
+       
+    }
+}
